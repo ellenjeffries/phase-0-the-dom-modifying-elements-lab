2cf70fd1d1bb3e99a6228cd7a4f94eab87855b94
ellen <ellenjojeffries@gmail.com>
2024-05-11 15:31:29 -0600

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const newHeader = document.createElement('h1');
+
+//document.addEventListener("DOMContentLoaded", function() {
+    const mainNode = document.querySelector("main#main");
+    mainNode.remove("main");
+  
+    newHeader.id = 'victory';
+    const yourName = "Ellen Jeffries";
+    newHeader.textContent = "${yourName} is the champion";
+  
+    document.body.appendChild(newHeader);
+//});
